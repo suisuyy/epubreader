022df46325aa381688de7405c62421535a0b50a7
chuyalong <chuyalong@example.com>
2020-07-08 16:21:18 +0000

    feat: add youdao online dict,but not well on desktop because redirect automatically;can move dict up or down now;close dict from Dict view

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default class App extends React.Component {
             onlineBookNames: [],
             onlineDictNames: [],
             ifShowNavBar: true,
-            ifShowDict: false,
+            ifShowDict: true,
             word: '',
             wordFromBook: '',
         };
@@ -42,78 +42,74 @@ export default class App extends React.Component {
 
                 <Route exact path='/' render={({ history }) => (
                     <div className="home">
-                            <input id="fileElem" className='noborder visually-hidden' type='file' onChange={(e) => this.handleFileInput(e)} />
-                            <label htmlFor="fileElem">click me to Select book from your computer</label>
-                            <Shelf booknames={this.state.bookfiles.map(i => i.name)}
-                                name='local'
-                                openBook={(filename) => {
-                                    this.openBook(filename);
-                                    history.push('/reader');
-                                }}
-                                uploadBook={(name) => this.uploadFile(name)}
-                                rmFile={(name) => this.rmFile(name)}
-                            />
-                            <Shelf name='online'
-                                booknames={this.state.onlineBookNames}
-                                downloadBook={(name) => this.downloadFile(name)}
-                            />
-                            <Shelf name='dict'
-                                booknames={this.state.onlineDictNames}
-                                downloadBook={(name) => this.downloadFile(name)}
-                            />
+                        <input id="fileElem" className='noborder visually-hidden' type='file' onChange={(e) => this.handleFileInput(e)} />
+                        <label htmlFor="fileElem">click me to Select book from your computer</label>
+                        <Shelf booknames={this.state.bookfiles.map(i => i.name)}
+                            name='local'
+                            openBook={(filename) => {
+                                this.openBook(filename);
+                                history.push('/reader');
+                            }}
+                            uploadBook={(name) => this.uploadFile(name)}
+                            rmFile={(name) => this.rmFile(name)}
+                        />
+                        <Shelf name='online'
+                            booknames={this.state.onlineBookNames}
+                            downloadBook={(name) => this.downloadFile(name)}
+                        />
+                        <Shelf name='dict'
+                            booknames={this.state.onlineDictNames}
+                            downloadBook={(name) => this.downloadFile(name)}
+                        />
 
                     </div>
                 )} />
 
-                        <Route path='/reader' render={() => (
-                            <EpubView file={this.state.currentFile} setWordFromBook={(word) => this.setState({ wordFromBook: word })} />
-                        )} />
+                <Route path='/reader' render={() => (
+                    <EpubView file={this.state.currentFile} setWordFromBook={(word) => this.setState({ wordFromBook: word })} />
+                )} />
 
-                        {
-                            this.state.dictfiles.length <= 0 ?
-                                <div className='dict' style={{ display: this.state.ifShowDict ? 'inherit' : 'none' }}>
-                                    No dict file found, download it now!
-                        </div>
-                                :
-                                <Dict
-                                    wordFromBook={this.state.wordFromBook}
-                                    dictfiles={this.state.dictfiles}
-                                    style={{ display: this.state.ifShowDict ? 'inherit' : 'none' }}
-                                />
-                        }
-                        {/* <Route path='/dict' render={() => {
+
+                <Dict
+                    wordFromBook={this.state.wordFromBook}
+                    dictfiles={this.state.dictfiles}
+                    style={{ display: this.state.ifShowDict ? 'inherit' : 'none' }}
+                    toggleDict={()=>this.toggleDict()}
+                />
+
+                {/* <Route path='/dict' render={() => {
                     if (this.state.dictfiles.length <= 0) {
                         return (<p>loading dict ...</p>)
                     }
                     console.log('dict load:', Array.isArray(this.state.dictfiles))
                     return <Dict dictfiles={this.state.dictfiles} />
                 }} /> */}
-                    </div>
-                );
+            </div>
+        );
     }
 
     componentDidMount() {
-                    getAllBookNamesOnline().then((data) => {
-                        this.setState({
-                            onlineBookNames: data,
-                        })
-                    });
+        getAllBookNamesOnline().then((data) => {
+            this.setState({
+                onlineBookNames: data,
+            })
+        });
         getFileNamesOnline('json').then(data => {
-                    this.setState({
-                        onlineDictNames: data,
-                    })
-                });
+            this.setState({
+                onlineDictNames: data,
+            })
+        });
         getAllFiles((event) => {
-                    let files = event.target.result;
+            let files = event.target.result;
             console.log(files)
             let epubfiles = files.filter(f => f.name.endsWith('epub'));
             let dictfiles = files.filter(f => f.name.endsWith('json'));
             if (Array.isArray(dictfiles) !== true) {
-                    console.log('not aray');
+                console.log('not aray');
                 dictfiles = [dictfiles]
             }
             this.setState({
-                    bookfiles: epubfiles,
+                bookfiles: epubfiles,
                 dictfiles: dictfiles
             });
         });
@@ -121,75 +117,75 @@ export default class App extends React.Component {
 
 
     handleFileInput(e) {
-                    let file = e.target.files[0];
+        let file = e.target.files[0];
         if (file.name.endsWith('json')) {
-                    this.setState({
-                        dictfiles: this.state.dictfiles.concat(file)
-                    });
+            this.setState({
+                dictfiles: this.state.dictfiles.concat(file)
+            });
         }
         else {
-                    this.setState({
-                        currentFile: file,
-                        bookfiles: this.state.bookfiles.concat(file)
-                    });
+            this.setState({
+                currentFile: file,
+                bookfiles: this.state.bookfiles.concat(file)
+            });
         }
 
         savefile(file);
     }
 
     downloadFile(name) {
-                    getRemoteFile(name).then(resBlob => {
-                        let file = new File([resBlob], name);
-                        if (name.endsWith('.epub')) {
-                            this.setState({
-                                bookfiles: this.state.bookfiles.concat(file)
-                            })
-                        }
-                        if (name.endsWith('json')) {
-                            this.setState({
-                                dictfiles: [file].concat(this.state.dictfiles),
-                            })
-                        }
-                        savefile(file);
+        getRemoteFile(name).then(resBlob => {
+            let file = new File([resBlob], name);
+            if (name.endsWith('.epub')) {
+                this.setState({
+                    bookfiles: this.state.bookfiles.concat(file)
+                })
+            }
+            if (name.endsWith('json')) {
+                this.setState({
+                    dictfiles: [file].concat(this.state.dictfiles),
+                })
+            }
+            savefile(file);
 
-                    })
-                }
+        })
+    }
     rmFile(name) {
 
         if (name.endsWith('.epub')) {
-                    this.setState({
-                        bookfiles: this.state.bookfiles.filter(item => item.name !== name)
-                    })
-                }
+            this.setState({
+                bookfiles: this.state.bookfiles.filter(item => item.name !== name)
+            })
+        }
         if (name.endsWith('json')) {
-                    this.setState({
-                        dictfiles: this.state.dictfiles.filter(item => item.name !== name),
-                    })
-                }
+            this.setState({
+                dictfiles: this.state.dictfiles.filter(item => item.name !== name),
+            })
+        }
         rmFiles([name]);
 
     }
 
     uploadFile(name) {
-                    uploadFile(name).then(() => {
-                        if (name.endsWith('.epub')) {
-                            this.setState({
-                                onlineBookNames: this.state.onlineBookNames.concat(name)
-                            })
-                        }
-                    }
-                    )
-                }
+        uploadFile(name).then(() => {
+            if (name.endsWith('.epub')) {
+                this.setState({
+                    onlineBookNames: this.state.onlineBookNames.concat(name)
+                })
+            }
+        }
+        )
+    }
     openBook(filename) {
-                    this.setState({
-                        currentFile: this.state.bookfiles.filter(i => i.name === filename)[0],
-                    });
+        this.setState({
+            currentFile: this.state.bookfiles.filter(i => i.name === filename)[0],
+        });
     }
 
     toggleDict() {
-                    console.log('toggledict' + document.getSelection().toString())
+        console.log('toggledict' + document.getSelection().toString())
         this.setState({
-                    word: document.getSelection().toString(),
+            word: document.getSelection().toString(),
             ifShowDict: !this.state.ifShowDict,
         })
     }
--- src/componets/Dict.js
+++ src/componets/Dict.js
@@ -7,63 +7,100 @@ export default class Dict extends React.Component {
         this.state = {
             word: this.props.wordFromBook,
             defs: [],
-            dict:{}
+            dict: {},
+            style:{}
         };
     }
     componentWillReceiveProps(nextProps) {
         //if your props is received after the component is mounted, then this function will update the state accordingly.
-        if(this.props.wordFromBook !== nextProps.wordFromBook) {
-         this.setState({word: nextProps.wordFromBook});
+        if (this.props.wordFromBook !== nextProps.wordFromBook) {
+            this.setState({ word: nextProps.wordFromBook });
         }
-      }
+    }
     render() {
-        
         return (
-            <div className='dict' style={this.props.style}>
-            {this.props.dictfiles.length===0 && <p>no dict file found you need download first</p>}
-            <p>dict file name: {this.props.dictfiles[0].name}</p>
+            <div className='dict' style={{...this.props.style,...this.state.style}}>
+                <div>
+                <a href="#localdict"> <button className="dictbutton">Local</button> </a>
+                <a href="#yddict"> <button className="dictbutton">YouDao</button> </a>
+                <button onClick={()=>this.moveUp()} className="dictbutton">up</button>
+                <button onClick={()=>this.moveDown()} className="dictbutton">down</button>
+                <button onClick={()=>this.props.toggleDict() } className="dictbutton">close</button>
+
+
+                </div>
                 
-                <form onSubmit={(event) => {
-                    event.preventDefault();
-                    this.search(this.state.word);
-                }}>
-                    <input type='text' value={this.state.word} onChange={(event) => this.handleInput(event)} />
-                    <button onClick={() => this.search(this.state.word)}>search</button>
-                    {/* <p onClick={()=>this.search(this.props.word)}>click me to search: {this.props.word}</p> */}
-                </form>
-            <p>{this.props.wordFromBook }</p>
-                <div className='defview'>
-                    <ol>
-                        
-                        {this.state.word.length>1&&this.state.dict[this.state.word]&&this.state.dict[this.state.word].map((item) => {
-                            return <li key={Math.random()}>
+                {this.props.dictfiles.length !== 0 &&
+                    <div id="localdict">
+                        <p>dict file name: {this.props.dictfiles[0].name}</p>
+
+                        <form onSubmit={(event) => {
+                            event.preventDefault();
+                            this.search(this.state.word);
+                        }}>
+                            <input type='text' value={this.state.word} onChange={(event) => this.handleInput(event)} />
+                            <button onClick={() => this.search(this.state.word)}>search</button>
+                            {/* <p onClick={()=>this.search(this.props.word)}>click me to search: {this.props.word}</p> */}
+                        </form>
+                        <p>{this.props.wordFromBook}</p>
+                        <div className='defview'>
+                            <ol>
+
+                                {this.state.word.length > 1 && this.state.dict[this.state.word] && this.state.dict[this.state.word].map((item) => {
+                                    return <li key={Math.random()}>
                                         <p>{item}</p>
                                     </li>
-                        })}
-                    </ol>
-                </div>
+                                })}
+                            </ol>
+                        </div>
+                    </div>
+
+                }
+                
+                <iframe title="youdao" 
+                id="yddict"
+                src={`https://m.youdao.com/dict?q=${this.state.word}#collins` }
+                />
+
+
             </div>
+
         )
     }
 
-    componentDidMount(){
-        let fr=new FileReader();
-        fr.onload=(e)=>{
-            let result=e.target.result;
+    componentDidMount() {
+        let fr = new FileReader();
+        fr.onload = (e) => {
+            let result = e.target.result;
             this.setState({
                 dict: JSON.parse(result),
-            })   
+            })
         }
-        if(this.props.dictfiles.length===0){
+        if (this.props.dictfiles.length === 0) {
             return
         }
         fr.readAsText(this.props.dictfiles[0]);
-        document.onselectionchange=()=>{
-            let word=document.getSelection().toString();
-            if(word.length>1){
+        document.onselectionchange = () => {
+            let word = document.getSelection().toString();
+            if (word.length > 1) {
                 this.search(word);
             }
         };
+    }
+
+    moveUp(){
+        this.setState({
+            style:{
+                top: 0
+            }
+        })
+    }
+    moveDown(){
+        this.setState({
+            style:{
+                bottom: 0
+            }
+        })
     }
 
     handleInput(event) {
@@ -78,7 +115,7 @@ export default class Dict extends React.Component {
             defs = [`sorry no definition for the word '${word}'`]
         }
         this.setState({
-            word:word,
+            word: word,
             defs: defs,
         })
     }
